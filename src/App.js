a4425b5a8083132b9bfa8f50157521c28ace3bce
vipin <pvipin907@gmail.com>
2021-06-26 11:00:04 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import Header from './Components/Header';
+import Home from './Components/Home';
 function App() {
   return (
     <div className="App">
-      hello
+      <Header />
+      <Home />
     </div>
   );
 }
