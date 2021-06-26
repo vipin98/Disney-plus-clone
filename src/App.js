e174ef680947451b3f38cb27200d9b2ac8479136
vipin <pvipin907@gmail.com>
2021-06-26 18:11:59 +0530

    Login and movies

--- src/App.js
+++ src/App.js
@@ -2,11 +2,34 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Home from './Components/Home';
+import Detail from './Components/Detail';
+import Login from './Components/Login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/detail">
+            <Detail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+
+
     </div>
   );
 }
